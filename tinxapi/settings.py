46b7c78fb3213ecd71c8315e4eaba703de860ba8
Daniel Cannon <cannon.danielc@gmail.com>
2018-08-29 17:33:05 -0600

    Add mysql database

--- tinxapi/settings.py
+++ tinxapi/settings.py
@@ -77,6 +77,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'tcrd': {
+        'ENGINE': 'django.db.backends.mysql',
+        'USER':   'tcrd_read_only',
+        'PASSWORD': 'tcrd_read_only'
     }
 }
 
