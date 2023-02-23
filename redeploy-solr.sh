73b3171c526674461361c8da1a0040ccdcad7b2a
Justin Edwards <jedwards@elevatodigital.com>
2023-02-23 16:53:30 +0000

    Merged in solr-work (pull request #33)
    Solr work
    
    * solr work
    
    * solr work
    
    * making the redeploy better
    
    * resolving merge
    
    
    Approved-by: Daniel Cannon

--- redeploy-solr.sh
+++ redeploy-solr.sh
@@ -3,13 +3,14 @@ set -x
 set -e
 
 echo "trying to deploy the solr container"
-echo "You may need to delete some crufty old container"
+docker stop tinx-solr
+docker rm tinx-solr
 docker run -d -p 8983:8983 --name tinx-solr solr:6.6.6
 docker exec -it tinx-solr mkdir /opt/solr/server/solr/haystack
 docker exec -it tinx-solr cp -r server/solr/configsets/basic_configs/conf /opt/solr/server/solr/haystack
 docker cp tinxapi/managed-schema tinx-solr:/opt/solr/server/solr/haystack/conf/managed-schema
+docker exec -it tinx-solr solr create_core -c haystack -p 8983
 echo "deployed solr container successfully"
-echo "visit the SOLR admin panel (http://35.86.241.213:8983/) and create a core named haystack"
-
-
-
+echo "You may visit the SOLR admin panel here: http://35.86.241.213:8983/"
+echo "updating indices"
+./update_solr_index.sh
