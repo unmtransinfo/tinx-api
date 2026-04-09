68e66144bc9584f7e40e916d09a93bdb77019de3
Jack-42 <97483169+Jack-42@users.noreply.github.com>
2026-04-09 16:33:28 -0600

    update solr URL

--- tinxapi/tinxapi/settings.py
+++ tinxapi/tinxapi/settings.py
@@ -148,12 +148,12 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
-HAYSTACK_SOLR_URL = "http://tinx-solr:8983/solr"
+HAYSTACK_SOLR_URL = "http://solr:8983/solr"
 
 HAYSTACK_CONNECTIONS = {
     "default": {
         "ENGINE": "haystack.backends.solr_backend.SolrEngine",
-        "URL": "http://tinx-solr:8983/solr/haystack",
+        "URL": "http://solr:8983/solr/haystack",
         "INDEX_NAME": "haystack",
     },
 }
